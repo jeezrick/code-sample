8dd8c84cbd7dda4600965744e6ebec1250780ce3
jeezrick <47274616+jeezrick@users.noreply.github.com>
2022-08-09 09:57:13 +0800

    Delete GA.py

--- GA.py
+++
@@ -1,7 +0,0 @@
-def generate_array(size, shape, random=False):
-    import numpy as np
-    arr = np.linspace(1, size, size)
-    arr = arr.reshape(shape)
-    if random:
-        np.random.shuffle(arr)
-    return arr
